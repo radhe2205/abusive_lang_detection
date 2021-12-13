f0913a6b0e774435a8e07978af3611d90264dccf
rowlavel <rowlavel@iu.edu>
2021-12-13 13:46:05 -0500

    cleaned up code

--- transfer_learning/categorical_lstm.py
+++ transfer_learning/categorical_lstm.py
@@ -33,49 +33,6 @@ class CatLSTM(nn.Module):
 
         self.linear_layers = nn.Sequential(
             nn.BatchNorm1d(self.hidden_size * 2),
-            nn.Linear(self.hidden_size * 2, self.hidden_size * 2),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-
-            nn.BatchNorm1d(self.hidden_size * 2),
-            nn.Linear(self.hidden_size * 2, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-
-            nn.BatchNorm1d(256),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-
-            nn.BatchNorm1d(128),
-            nn.Linear(128, self.out_dim)
-        )
-
-        # Weight Initialization
-        for layer in self.linear_layers:
-            if type(layer) == nn.Linear:
-                torch.nn.init.xavier_normal_(layer.weight.data)
-
-    # number of words in tweet are limited, will use padded fixed length sequence.
-    def forward(self, samples):
-        word_embs = self.embeddings.get_embeddings(samples)
-        o, (h,c) = self.rnn(word_embs)
-        o = torch.cat((o[:,-1,:self.hidden_size], o[:, 0, self.hidden_size:]), dim=-1)
-        return self.linear_layers(o).squeeze()
-
-class CatLSTM(nn.Module):
-    def __init__(self, embeddings, in_dim, num_layers = 1, hidden_size = 100, out_dim = 1):
-        super(CatLSTM, self).__init__()
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.out_dim = out_dim
-
-        self.embeddings = embeddings
-
-        self.rnn = nn.LSTM(input_size=in_dim, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, bidirectional=True, dropout=0.5)
-
-        self.linear_layers = nn.Sequential(
-            nn.BatchNorm1d(self.hidden_size * 2),
             nn.Linear(self.hidden_size * 2, self.out_dim),
         )
 
